d61c3adaf85df2c2f9082d6a68f080fee3f655c6
Hasan Demirtaş <utsukushihito@outlook.com>
2022-11-03 21:34:23 +0300

    Update PingListener.java

--- src/main/java/io/github/shiruka/network/server/pipelines/PingListener.java
+++ src/main/java/io/github/shiruka/network/server/pipelines/PingListener.java
@@ -3,7 +3,6 @@ package io.github.shiruka.network.server.pipelines;
 import io.github.shiruka.network.options.RakNetConfig;
 import io.github.shiruka.network.packets.UnconnectedPing;
 import io.github.shiruka.network.packets.UnconnectedPong;
-import io.github.shiruka.network.server.RakNetServer;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.util.ReferenceCountUtil;
 import java.net.InetSocketAddress;
@@ -31,19 +30,15 @@ public final class PingListener extends UdpPacketHandler<UnconnectedPing> {
   protected void handle(
     final ChannelHandlerContext ctx,
     final InetSocketAddress sender,
-    final UnconnectedPing ping
+    final UnconnectedPing packet
   ) {
     final var config = RakNetConfig.cast(ctx);
-    final var connections = RakNetServer.cast(ctx).children();
-    if (connections.size() >= config.maxConnections()) {
-      return;
-    }
-    ReferenceCountUtil.retain(ping);
+    ReferenceCountUtil.retain(packet);
     final var pong = new UnconnectedPong(
       config.serverIdentifier(),
       config.magic(),
       config.serverId(),
-      ping.timestamp()
+      packet.timestamp()
     );
     PingListener.sendResponse(ctx, sender, pong);
   }
